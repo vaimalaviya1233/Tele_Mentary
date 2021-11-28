55aa449171417cfb20367f615f176b04645e31d0
vaibhav malaviya <vaibhavmalaviya@yahoo.com>
2021-11-28 14:30:46 -0600

    git added
    Signed-off-by: vaibhav malaviya <Vaibhavmalaviya@yahoo.com>

--- app/src/main/java/com/vaibhav/telementary/MyAdapter.java
+++ app/src/main/java/com/vaibhav/telementary/MyAdapter.java
@@ -1,13 +1,9 @@
 package com.vaibhav.telementary;
-import android.app.PendingIntent;
+
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +15,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.io.IOException;
-import java.util.Comparator;
-import java.util.List;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     Context context;
     PackageManager packageManager;
@@ -49,9 +41,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         main_view.info_more.setOnClickListener(v -> context.startActivity(app.infoIntent.get(position)));
         main_view.play_store.setOnClickListener(v -> context.startActivity(app.playstore.get(position)));
         main_view.icon.setOnClickListener(v -> Toast.makeText(context, "Launching App Not Supported Right Now!", Toast.LENGTH_SHORT).show());
-        if(position == app.packageSize){
+        /*if(position == app.packageSize){
             app.releaser();
-        }
+        }*/
         main_view.delete.setOnClickListener(v -> context.startActivity (new Intent(Intent.ACTION_UNINSTALL_PACKAGE).setData(Uri.parse("package:"+app.pkgNames.get(position)))));
         main_view.suspend.setOnClickListener(v -> {
 //            Runtime rnt = Runtime.getRuntime();
@@ -62,10 +54,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 //                Toast.makeText(context,e.toString(),Toast.LENGTH_LONG).show();//debug version only
 //                //Toast.makeText(context, "Error while Suspending App",Toast.LENGTH_SHORT).show();
 //            }
-            packageManager.setApplicationEnabledSetting(packageAdd,packageManager.COMPONENT_ENABLED_STATE_DISABLED,packageManager.SYNCHRONOUS);
+            packageManager.setApplicationEnabledSetting(packageAdd, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.SYNCHRONOUS);
         });
         main_view.unsuspend.setOnClickListener(v -> {
-            packageManager.setApplicationEnabledSetting(packageAdd, packageManager.COMPONENT_ENABLED_STATE_ENABLED, packageManager.SYNCHRONOUS);
+            packageManager.setApplicationEnabledSetting(packageAdd, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.SYNCHRONOUS);
         });
     }
 
